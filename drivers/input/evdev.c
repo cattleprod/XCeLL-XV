e92526ac7459ab2df09b4faaa2324cf8baa7949f
craig <hot2009wheels@hotmail.com>
2011-12-08 23:05:35 +0000

    Reverted commit 2137ce1ea9a01828dea534066f44c07637e41900

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -86,7 +86,6 @@ static void evdev_event(struct input_handle *handle,
 	event.code = code;
 	event.value = value;
 
-	sched_wake_interactive_enable();
 	rcu_read_lock();
 
 	client = rcu_dereference(evdev->grab);
@@ -99,7 +98,6 @@ static void evdev_event(struct input_handle *handle,
 	rcu_read_unlock();
 
 	wake_up_interruptible(&evdev->wait);
-	sched_wake_interactive_disable();
 }
 
 static int evdev_fasync(int fd, struct file *file, int on)
