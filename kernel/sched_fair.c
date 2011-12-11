27b827b4b06c000f398afdd20df4ce4c8b029ca3
craig <hot2009wheels@hotmail.com>
2011-12-11 23:22:17 +0000

    sched input interactivity-driven next buddy
    [ Impact: implement INTERACTIVE feature to increase Xorg responsiveness. ] Apply next buddy logic to interactivity-driven wakeups. Don't pass the interactivity flag across forks to defuse interactivity-based fork-bombs. The goal of this patch is to ensure that Xorg keeps a good interactivity level by ensuring that Xorg and its related threads quickly respond to wakeups caused by user inputs. Derived from a patch from Peter Zijlstra. * This patch also makes sure that as soon as an iowait is perceived, the interactivity chain is stopped. * This patch removes the previously available "NEXT_BUDDY" scheduler feature altogether. On my 2.0GHz uniprocessor desktop, enabling the INTERACTIVE feature makes firefox very responsive even if I overcommit my CPU with a make -j5 kernel build. Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com> CC: Peter Zijlstra <a.p.zijlstra@chello.nl> Signed-off-by: Brandon Berhent <bbedward@gmail.com>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -738,7 +738,9 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		vruntime += sched_vslice(cfs_rq, se);
 
 	/* sleeps up to a single latency don't count. */
-	if (!initial) {
+	if (!initial
+		&& (sched_feat(FAIR_SLEEPERS)
+		   || (sched_feat(FAIR_SLEEPERS_INTERACTIVE) && se->interactive))) {
 		unsigned long thresh = sysctl_sched_latency;
 
 		/*
@@ -774,6 +776,9 @@ enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	account_entity_enqueue(cfs_rq, se);
 
 	if (flags & ENQUEUE_WAKEUP) {
+		if (sched_feat(INTERACTIVE)
+			&& flags & ENQUEUE_LATENCY && !(flags & ENQUEUE_IO))
+				se->interactive = 1;
 		place_entity(cfs_rq, se, 0);
 		enqueue_sleeper(cfs_rq, se);
 	}
@@ -915,14 +920,14 @@ static struct sched_entity *pick_next_entity(struct cfs_rq *cfs_rq)
 	struct sched_entity *se = __pick_next_entity(cfs_rq);
 	struct sched_entity *left = se;
 
-	if (cfs_rq->next && wakeup_preempt_entity(cfs_rq->next, left) < 1)
-		se = cfs_rq->next;
-
-	/*
-	 * Prefer last buddy, try to return the CPU to a preempted task.
-	 */
 	if (cfs_rq->last && wakeup_preempt_entity(cfs_rq->last, left) < 1)
 		se = cfs_rq->last;
+
+	/*
+	 * Prefer the next buddy, only set through the interactivity logic.
+	 */
+	if (cfs_rq->next && wakeup_preempt_entity(cfs_rq->next, left) < 1)
+		se = cfs_rq->next;
 
 	clear_buddies(cfs_rq, se);
 
@@ -1046,6 +1051,9 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
+	
+	if (p->in_iowait)
+		flags |= ENQUEUE_IO;
 
 	for_each_sched_entity(se) {
 		if (se->on_rq)
@@ -1658,6 +1666,7 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	 * tasks for there to be buddies.
 	 */
 	int buddies = (cfs_rq->nr_running >= 2);
+	int preempt = 0;
 
 	if (unlikely(rt_prio(p->prio)))
 		goto preempt;
@@ -1668,8 +1677,13 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	if (unlikely(se == pse))
 		return;
 
-	if (sched_feat(NEXT_BUDDY) && buddies && !(wake_flags & WF_FORK))
+	if (sched_feat(INTERACTIVE)
+		&& !(wake_flags & WF_FORK) && pse->interactive) {
+		clear_buddies(cfs_rq, NULL);
 		set_next_buddy(pse);
+		preempt = 1;
+		buddies = 0;
+	}
 
 	/*
 	 * We can come here with TIF_NEED_RESCHED already set from new task
@@ -1695,7 +1709,7 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	update_curr(cfs_rq);
 	find_matching_se(&se, &pse);
 	BUG_ON(!pse);
-	if (wakeup_preempt_entity(se, pse) == 1)
+	if (preempt || wakeup_preempt_entity(se, pse) == 1)
 		goto preempt;
 
 	return;
