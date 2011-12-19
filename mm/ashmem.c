1d59e45d69942b35b91bbbaad94c9428c8c9ac9c
craig <hot2009wheels@hotmail.com>
2011-12-19 19:24:41 +0000

    ashmem: Fix flush routine of ashmem
    The ashmem flush function ashmem_flush_cache_range was calling
    dmac_clean_range and dmac_flush_range with a user virtual address
    which is not allowed. Instead of calling dmac_clean_range or
    dmac_flush_range use flush_cache_user_range which can be called
    with a user vitual address.
    
    Change-Id: I5dd137fb248b45ef2c7ea5f7bdb4b540b8310af9
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>

--- mm/ashmem.c
+++ mm/ashmem.c
@@ -680,7 +680,7 @@ static unsigned int kgsl_virtaddr_to_physaddr(unsigned int virtaddr)
 }
 #endif
 
-static int ashmem_flush_cache_range(struct ashmem_area *asma, unsigned long cmd)
+static int ashmem_flush_cache_range(struct ashmem_area *asma)
 {
 #ifdef CONFIG_OUTER_CACHE
 	unsigned long end;
@@ -698,19 +698,7 @@ static int ashmem_flush_cache_range(struct ashmem_area *asma, unsigned long cmd)
 		goto done;
 	}
 	
-	switch (cmd) {
-	case ASHMEM_CACHE_FLUSH_RANGE:
-		dmac_flush_range((const void *)addr,
-			(const void *)(addr + size));
-		break;
-	case ASHMEM_CACHE_CLEAN_RANGE:
-		dmac_clean_range((const void *)addr,
-			(const void *)(addr + size));
-		break;
-	defaulf:
-		result = -EINVAL;
-		goto done;
-	}
+	flush_cache_user_range(addr, addr + size);
 #ifdef CONFIG_OUTER_CACHE
 	for (end = addr; end < (addr + size); end += PAGE_SIZE) {
 		unsigned long physaddr;
@@ -720,14 +708,7 @@ static int ashmem_flush_cache_range(struct ashmem_area *asma, unsigned long cmd)
 			goto done;
 		}
 		
-		switch (cmd) {
-		case ASHMEM_CACHE_FLUSH_RANGE:
-			outer_flush_range(physaddr, physaddr + PAGE_SIZE);
-			break;
-		case ASHMEM_CACHE_CLEAN_RANGE:
-			outer_clean_range(physaddr, physaddr + PAGE_SIZE);
-			break;
-		}
+		outer_flush_range(physaddr, physaddr + PAGE_SIZE);
 	}
 #endif
 done:
@@ -776,8 +757,7 @@ static long ashmem_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		}
 		break;
 	case ASHMEM_CACHE_FLUSH_RANGE:
-	case ASHMEM_CACHE_CLEAN_RANGE:
-		ret = ashmem_flush_cache_range(asma, cmd);
+		ret = ashmem_flush_cache_range(asma);
 		break;
 	}
 
